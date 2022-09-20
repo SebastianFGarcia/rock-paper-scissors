425a13bb3e9a08f18c1dceb4249b5661ad3d1ba5
Sebastian Garcia <sfernandogarcia@uts.edu.co>
2022-09-20 10:00:39 -0500

    achivo con funciones

--- main.js
+++ main.js
@@ -3,56 +3,40 @@ let player = 0
 let countCPU = 0
 let countPlayer = 0
 let salio = true
+
+function textVersus(jugador) {
+    if (jugador == 1)  return "Rock"
+    else if (jugador == 2) return "Paper"
+    else if (jugador == 3) return "Scissors"
+}
+
+function Versus(jugador, cpu) {
+    if (jugador == cpu) return "Draw"
+    else if (jugador == 1 && cpu == 2) return "CPU win!" + countCPU++
+    else if (jugador == 1 && cpu == 3) return "Player win!" + countPlayer++
+    else if (jugador == 2 && cpu == 1) return "Player win!" + countPlayer++
+    else if (jugador == 2 && cpu == 3) return "CPU win!" + countCPU++
+    else if (jugador == 3 && cpu == 1) return "CPU win!" + countCPU++
+    else if (jugador == 3 && cpu == 2) return "Player win!" + countPlayer++
+}
+function salir(jugador,numero) {
+    if (numero == 3) {
+        salio = false
+        alert(jugador+" wins the game!")
+    }
+}
+
+
 while (salio) {
     cpu = Math.floor(Math.random() * 3) + 1
+
     player = parseInt(prompt("1. Rock\n2. Paper\n3. Scissors"))
-    let result = ""
-    if (player == 1) {
-        result = "Rock"
-    } else if (player == 2) {
-        result = "Paper"
-    } else if (player == 3) {
-        result = "Scissors"
-    }
-    if (cpu == 1) {
-        result += " vs Rock"
-    } else if (cpu == 2) {
-        result += " vs Paper"
-    } else if (cpu == 3) {
-        result += " vs Scissors"
-    }
+
+    let result = (textVersus(player) + " vs " + textVersus(cpu))
     alert(result)
-    if (cpu == 1 && player == 2) {
-        countPlayer++
-        alert('Player win!' + ' CPU: ' + countCPU + 'Player: ' + countPlayer)
-    } else if (cpu == 1 && player == 3) {
-        countCPU++
-        alert('CPU win!' + ' CPU: ' + countCPU + 'Player: ' + countPlayer)
-    } else if (cpu == 2 && player == 1) {
-        countCPU++
-        alert('CPU win!' + ' CPU: ' + countCPU + 'Player: ' + countPlayer)
-    }
-    else if (cpu == 2 && player == 3) {
-        countPlayer++
-        alert('Player win!' + ' CPU: ' + countCPU + 'Player: ' + countPlayer)
-    }
-    else if (cpu == 3 && player == 1) {
-        countPlayer++
-        alert('Player win!' + ' CPU: ' + countCPU + 'Player: ' + countPlayer)
-    }
-    else if (cpu == 3 && player == 2) {
-        countCPU++
-        alert('CPU win!' + ' CPU: ' + countCPU + 'Player: ' + countPlayer)
-    }
-    else if (cpu == player) {
-        alert('Draw!' + ' CPU: ' + countCPU + 'Player: ' + countPlayer)
-    }
-    if (countCPU == 3) {
-        alert('CPU win the game!')
-        salio = false
-    }
-    else if (countPlayer == 3) {
-        alert('Player win the game!')
-        salio = false
-    }
+    alert(Versus(player, cpu))
+    alert("Player: " + countPlayer + " CPU: " + countCPU)
+
+    salir("Player",countPlayer)
+    salir('CPU',countCPU)
 }
